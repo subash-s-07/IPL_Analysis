15166a6ea87e7982eb240eeaf99accfe5191f964
subash-s-07 <121507095+subash-s-07@users.noreply.github.com>
2023-12-20 19:49:07 +0530

    Update 7_❤️_PKBS.py

--- pages/7_❤️_PKBS.py
+++ pages/7_❤️_PKBS.py
@@ -7,9 +7,6 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-
-img = get_img_as_base64("PKBS.jpg")
-
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
@@ -19,8 +16,6 @@ background-position: top left;
 background-repeat: no-repeat;
 background-attachment: local;
 }}
-[data-testid="stSidebar"] > div:first-child {{
-background-image: url("data:image/png;base64,{img}");
 .slider {{
     color: white;
 }}
